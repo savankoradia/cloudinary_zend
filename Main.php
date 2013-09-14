febb6b81baaa6253467c6b3afa5e275e060d4fe2
savan <sjk.frills2008@gmail.com>
2013-09-14 07:07:25 +0530

    Update Main.php

--- Main.php
+++ Main.php
@@ -1,11 +1,13 @@
 <?php
-
+#This is main file will be accessible from controller code.
+#These first 3 files are library of cloudinary.
 require_once('Cloudinary.php');
 require_once('Uploader.php');
 require_once('Api.php');
 
 class Cloudinary_Main {
 
+    #Cloudnary configuration settings will come through this function.
     public function __construct($settings) {
         $cloudinary = new Cloudinary();
         $config = $cloudinary->config(array(
@@ -16,6 +18,7 @@ class Cloudinary_Main {
         return $config;
     }
 
+    #upload image
     public function uploadImage($image) {
         $result = \Cloudinary\Uploader::upload($image);
         return $result;
